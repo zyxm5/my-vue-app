2b3bd29a9089e74a88bd6a6c6d27706b5786681d
zhiyang <zhiyang3370@dingtalk.com>
2021-03-01 15:24:53 +0800

    <perf>:(postcss.config.js) <添加插件地址>

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,13 @@
  * @Author: zyxm5
  * @Date: 2021-02-24 10:09:04
  * @LastEditors: zyxm5
- * @LastEditTime: 2021-03-01 15:06:57
+ * @LastEditTime: 2021-03-01 15:24:34
  * @Description:
  */
 module.exports = {
   map: false, //关闭source-map
   plugins: {
+    // https://github.com/csstools/postcss-preset-env
     "postcss-preset-env": {
       // 去掉兼容语法
       preserve: false,
@@ -15,6 +16,7 @@ module.exports = {
         "nesting-rules": true
       }
     },
+    // https://github.com/meltifa/postcss-pixel-to-viewport
     "postcss-pixel-to-viewport": {
       // 尺寸比例 原始尺寸 1920 * 1080
       viewportWidth: 1920,
