2992bd457ff76ad14089005510826e56b0dc96ee
zhiyang <zhiyang3370@dingtalk.com>
2021-03-01 16:58:26 +0800

    <feat>:(*) <集成vuex4>

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@
  * @Author: zyxm5
  * @Date: 2021-03-01 10:42:28
  * @LastEditors: zyxm5
- * @LastEditTime: 2021-03-01 16:46:28
+ * @LastEditTime: 2021-03-01 16:57:58
  * @Description: 
  */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import store from './store';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
