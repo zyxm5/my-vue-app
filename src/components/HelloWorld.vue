7e2c1165861ec58a4f5d596edfd5fb134ad3bc0d
zhiyang <zhiyang3370@dingtalk.com>
2021-03-02 10:17:19 +0800

    <feat>:(*) <集成eslint>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,43 +2,44 @@
  * @Author: zyxm5
  * @Date: 2021-03-01 10:42:28
  * @LastEditors: zyxm5
- * @LastEditTime: 2021-03-01 14:12:40
+ * @LastEditTime: 2021-03-02 10:05:07
  * @Description: 
 -->
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <h1>{{ msg }}</h1>
+    <h1>{{ book }}</h1>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const state = reactive({ count: 0 })
+<script>
+import { toRefs, ref, h, inject } from "vue";
+export default {
+    props: {
+        msg: {
+            type: String,
+            required: true
+        }
+    },
+    setup(props, context) {
+        // const count = ref(0);
+        // return () => h('div', count.value)
+        const book = inject('book')
+        return {
+            book
+        }
+    },
+    methods: {
+        changeCount(count) {
+            this.count = count;
+        }
+    }
+};
 </script>
-
 <style lang="postcss" scoped>
-@import url('@/css/index.css');
-p{
-  color: var(--mainColor);
-  & a{
-    color: #008c8c;
-  }
-  &::after{
-    content: '123'
-  }
+@import url("@/css/index.css");
+p {
+    color: var(--mainColor);
+    & a {
+        color: #008c8c;
+    }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
+<!--
+ * @Author: zyxm5
+ * @Date: 2021-03-01 15:28:45
+ * @LastEditors: zyxm5
+ * @LastEditTime: 2021-03-02 08:43:31
+ * @Description: 
+-->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <HelloWorld
+      :msg="msg"
+      @changeMsg="msg = $event"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+    name: "Home",
+    components: {
+        HelloWorld
+    },
+    data(){
+      return{
+        msg: "Welcome to Your Vue.js App"
+      }
+    }
 };
 </script>
